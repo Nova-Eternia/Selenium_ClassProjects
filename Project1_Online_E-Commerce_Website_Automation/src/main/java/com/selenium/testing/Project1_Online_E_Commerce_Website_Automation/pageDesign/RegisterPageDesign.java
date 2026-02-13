c1dbfa710e6df696947761e5fbe60c4cd58cc44c
mlaxmi1304 <mahalaxmipandey2004@gmail.com>
2026-02-13 15:45:13 +0530

    Updated registration test cases

--- Project1_Online_E-Commerce_Website_Automation/src/main/java/com/selenium/testing/Project1_Online_E_Commerce_Website_Automation/pageDesign/RegisterPageDesign.java
+++ Project1_Online_E-Commerce_Website_Automation/src/main/java/com/selenium/testing/Project1_Online_E_Commerce_Website_Automation/pageDesign/RegisterPageDesign.java
@@ -1,101 +1,154 @@
 package com.selenium.testing.Project1_Online_E_Commerce_Website_Automation.pageDesign;
 
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
-//import org.testng.Assert;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegisterPageDesign {
-	public WebDriver driver ;
-	
-	public RegisterPageDesign(WebDriver driver ){
-		this.driver = driver ;
-		PageFactory.initElements(driver, this); 
-	}
-	
-	@FindBy(xpath = "//label[normalize-space()='Male']")
-	WebElement male;
-	
-	@FindBy(xpath = "//label[normalize-space()='Female']" )
-	WebElement female;
-	
-	@FindBy(id = "FirstName")
-	WebElement firstName ;
-	
-	@FindBy(id = "LastName")
-	WebElement lastName;
-	
-	@FindBy(id = "Email" )
-	WebElement email;
-	
-	@FindBy(id = "Password")
-	WebElement pwd;
-	
-	@FindBy(id = "ConfirmPassword")
-	WebElement confirmPwd ;
-	
-	@FindBy(id = "register-button")
-	WebElement submitBtn;
-	
-	@FindBy(linkText = "Register")
-	WebElement RegisterLink;
-	
-	@FindBy(xpath = "//div[@class='result']")
-	WebElement verifyText;
-	
-	
-	@FindBy(css = "input[value='Continue']")
-	WebElement continueBtn;
-	//Methods
-	
-	public void chooseGenderMale() {
-		male.click();
-	}
-	
-	public void chooseGenderFemale() {
-		female.click();
-	}
-	
-	public void FirstName(String FirstName) {
-		firstName.sendKeys(FirstName);
-	}
-	
-	public void LastName(String LastName) {
-		lastName.sendKeys(LastName);
-	}
-	
-	public void fillEmail(String Email) {
-		email.sendKeys(Email);
-	}
-	
-	public void fillPwd(String Pwd) {
-		 pwd.sendKeys(Pwd);
-	}
-	
-	public void fillConfirmPwd(String ConfirmPwd) {
-		confirmPwd.sendKeys(ConfirmPwd);
-	}
-	
-	public void register() {
-		submitBtn.click();
-	}
-	
-	public void clickRegister() {
-		RegisterLink.click();
-	}
-	
-	public void verifyRegistration() {
-		String expectedStr = "Your registration completed";
-		String resultStr = verifyText.getText() ;
-		Assert.assertEquals(resultStr, expectedStr);
-	}
-	
-	public void clickContinue() {
-		continueBtn.click();
-	}
-	
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public RegisterPageDesign(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    // ================= LOCATORS =================
+
+    @FindBy(linkText = "Register")
+    WebElement registerLink;
+
+    @FindBy(id = "gender-female")
+    WebElement femaleGenderRadio;
+
+    @FindBy(id = "FirstName")
+    WebElement firstNameInput;
+
+    @FindBy(id = "LastName")
+    WebElement lastNameInput;
+
+    @FindBy(id = "Email")
+    WebElement emailInput;
+
+    @FindBy(id = "Password")
+    WebElement passwordInput;
+
+    @FindBy(id = "ConfirmPassword")
+    WebElement confirmPasswordInput;
+
+    @FindBy(id = "register-button")
+    WebElement registerButton;
+
+    @FindBy(css = ".result")
+    WebElement registrationSuccessMsg;
+
+    @FindBy(linkText = "Log out")
+    WebElement logoutLink;
+
+    @FindBy(css = "input[value='Continue']")
+    WebElement continueButton;
+
+    // Validation messages (correct DemoWebShop locators)
+
+    @FindBy(xpath = "//span[@data-valmsg-for='FirstName']")
+    WebElement firstNameError;
+
+    @FindBy(xpath = "//span[@data-valmsg-for='LastName']")
+    WebElement lastNameError;
+
+    @FindBy(xpath = "//span[@data-valmsg-for='Email']")
+    WebElement emailError;
+
+    @FindBy(xpath = "//span[@data-valmsg-for='Password']")
+    WebElement passwordError;
+
+    @FindBy(xpath = "//span[@data-valmsg-for='ConfirmPassword']")
+    WebElement confirmPasswordError;
+
+    @FindBy(xpath = "//div[@class='validation-summary-errors']")
+    WebElement duplicateEmailError;
+
+    // ================= ACTION METHODS =================
+
+    public void clickRegisterLink() {
+        wait.until(ExpectedConditions.elementToBeClickable(registerLink)).click();
+    }
+
+    public void selectFemaleGender() {
+        wait.until(ExpectedConditions.elementToBeClickable(femaleGenderRadio)).click();
+    }
+
+    public void enterFirstName(String firstName) {
+        firstNameInput.clear();
+        firstNameInput.sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastName) {
+        lastNameInput.clear();
+        lastNameInput.sendKeys(lastName);
+    }
+
+    public void enterEmail(String email) {
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    public void enterConfirmPassword(String confirmPassword) {
+        confirmPasswordInput.clear();
+        confirmPasswordInput.sendKeys(confirmPassword);
+    }
+
+    public void clickRegisterButton() {
+        registerButton.click();
+    }
+
+    public void clickContinueButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(continueButton)).click();
+    }
+
+    // ================= GETTERS =================
+
+    public String getRegistrationSuccessMessage() {
+        return wait.until(ExpectedConditions.visibilityOf(registrationSuccessMsg)).getText();
+    }
+
+    public boolean isUserLoggedIn() {
+        return wait.until(ExpectedConditions.visibilityOf(logoutLink)).isDisplayed();
+    }
+
+    public String getFirstNameError() {
+        return wait.until(ExpectedConditions.visibilityOf(firstNameError)).getText();
+    }
+
+    public String getLastNameError() {
+        return wait.until(ExpectedConditions.visibilityOf(lastNameError)).getText();
+    }
+
+    public String getEmailError() {
+        return wait.until(ExpectedConditions.visibilityOf(emailError)).getText();
+    }
+
+    public String getDuplicateEmailError() {
+        return wait.until(ExpectedConditions.visibilityOf(duplicateEmailError)).getText();
+    }
+
+    public String getPasswordMismatchError() {
+        return wait.until(ExpectedConditions.visibilityOf(confirmPasswordError)).getText();
+    }
+
+    public String getPasswordLengthError() {
+        return wait.until(ExpectedConditions.visibilityOf(passwordError)).getText();
+    }
 }
--- Project1_Online_E-Commerce_Website_Automation/src/test/java/com/selenium/testing/Project1_Online_E_Commerce_Website_Automation/testCases/RegisterPageTest.java
+++ Project1_Online_E-Commerce_Website_Automation/src/test/java/com/selenium/testing/Project1_Online_E_Commerce_Website_Automation/testCases/RegisterPageTest.java
@@ -1,78 +1,174 @@
 package com.selenium.testing.Project1_Online_E_Commerce_Website_Automation.testCases;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+
+
 import com.selenium.testing.Project1_Online_E_Commerce_Website_Automation.base.TestBase;
 import com.selenium.testing.Project1_Online_E_Commerce_Website_Automation.pageDesign.RegisterPageDesign;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.By;
-
-import org.testng.annotations.*;
-
 public class RegisterPageTest extends TestBase {
-	
-//	WebDriver driver = new ChromeDriver();
-	public RegisterPageDesign register;
-	
-	@Test
-	public void validRegistration() {
-		register = new RegisterPageDesign(driver);
-		register.clickRegister();
-		register.chooseGenderFemale();
-		register.FirstName("Riya");
-		register.LastName("Singh");
-		register.fillEmail("riyaSingh@example.com");
-		register.fillPwd("riyaSingh@123");
-		register.fillConfirmPwd("riyaSingh@123");
-		register.register();
-		register.verifyRegistration();
-		register.clickContinue();	
-	}
-	
-	@Test
-	public void invalidPassowrdRegistration() {
-		register = new RegisterPageDesign(driver);
-		register.clickRegister();
-		register.chooseGenderFemale();
-		register.FirstName("Riya");
-		register.LastName("Singh");
-		register.fillEmail("riyaSingh@example.com");
-		register.fillPwd("riyaSingh@123");
-		register.fillConfirmPwd("riyaSingh@123");
-		register.register();
-		register.verifyRegistration();
-		register.clickContinue();	
-	}
-	
-	@Test
-	public void sameEmailRegistration() {
-		register = new RegisterPageDesign(driver);
-		register.clickRegister();
-		register.chooseGenderFemale();
-		register.FirstName("Riya");
-		register.LastName("Singh");
-		register.fillEmail("riyaSingh@example.com");
-		register.fillPwd("riyaSingh@123");
-		register.fillConfirmPwd("riyaSingh@123");
-		register.register();
-		register.verifyRegistration();
-		register.clickContinue();	
-	}
-	
-	@Test
-	public void invalidRegistration() {
-		register = new RegisterPageDesign(driver);
-		register.clickRegister();
-		register.chooseGenderFemale();
-		register.FirstName("Riya");
-		register.LastName("Singh");
-		register.fillEmail("riyaSingh@example.com");
-		register.fillPwd("riyaSingh@123");
-		register.fillConfirmPwd("riyaSingh@123");
-		register.register();
-		register.verifyRegistration();
-		register.clickContinue();	
-	}
-}
 
+    RegisterPageDesign register;
+    
+    @BeforeMethod
+    public void setupTest() {
+        driver.get("https://demowebshop.tricentis.com/");
+
+        // If already logged in → logout
+        if (driver.findElements(By.linkText("Log out")).size() > 0) {
+            driver.findElement(By.linkText("Log out")).click();
+        }
+    }
+
+    // TC_REG_01 – Successful Registration
+    @Test
+    public void TC_REG_01_validRegistration() {
+
+        register = new RegisterPageDesign(driver);
+
+        register.clickRegisterLink();
+        register.selectFemaleGender();
+        register.enterFirstName("Hana");
+        register.enterLastName("Louis");
+        register.enterEmail("hana.louis" + System.currentTimeMillis() + "@testmail.com");
+        register.enterPassword("Test@123");
+        register.enterConfirmPassword("Test@123");
+        register.clickRegisterButton();
+
+        Assert.assertEquals(
+                register.getRegistrationSuccessMessage(),
+                "Your registration completed",
+                "Registration success message mismatch");
+
+        Assert.assertTrue(
+                register.isUserLoggedIn(),
+                "User is not logged in after registration");
+
+        register.clickContinueButton();
+    }
+
+    // TC_REG_02 – First Name Blank
+    @Test
+    public void TC_REG_02_firstNameBlank() {
+
+        register = new RegisterPageDesign(driver);
+
+        register.clickRegisterLink();
+        register.selectFemaleGender();
+        register.enterFirstName("");
+        register.enterLastName("Louis");
+        register.enterEmail("hana.blankfn" + System.currentTimeMillis() + "@testmail.com");
+        register.enterPassword("Test@123");
+        register.enterConfirmPassword("Test@123");
+        register.clickRegisterButton();
+
+        Assert.assertEquals(
+                register.getFirstNameError(),
+                "First name is required.",
+                "First name validation failed");
+    }
+
+    // TC_REG_03 – Last Name Blank
+    @Test
+    public void TC_REG_03_lastNameBlank() {
+
+        register = new RegisterPageDesign(driver);
+
+        register.clickRegisterLink();
+        register.selectFemaleGender();
+        register.enterFirstName("Hana");
+        register.enterLastName("");
+        register.enterEmail("hana.blankln" + System.currentTimeMillis() + "@testmail.com");
+        register.enterPassword("Test@123");
+        register.enterConfirmPassword("Test@123");
+        register.clickRegisterButton();
+
+        Assert.assertEquals(
+                register.getLastNameError(),
+                "Last name is required.",
+                "Last name validation failed");
+    }
+
+    // TC_REG_04 – Invalid Email
+    @Test
+    public void TC_REG_04_invalidEmail() {
+
+        register = new RegisterPageDesign(driver);
+
+        register.clickRegisterLink();
+        register.selectFemaleGender();
+        register.enterFirstName("Hana");
+        register.enterLastName("Louis");
+        register.enterEmail("hana.louis.com");
+        register.enterPassword("Test@123");
+        register.enterConfirmPassword("Test@123");
+        register.clickRegisterButton();
+
+        Assert.assertTrue(
+                register.getEmailError().contains("Wrong email"),
+                "Email validation failed");
+    }
+
+    // TC_REG_05 – Duplicate Email
+    @Test
+    public void TC_REG_05_duplicateEmail() {
+
+        register = new RegisterPageDesign(driver);
+
+        register.clickRegisterLink();
+        register.selectFemaleGender();
+        register.enterFirstName("Hana");
+        register.enterLastName("Louis");
+        register.enterEmail("hana.duplicate@testmail.com"); // already registered
+        register.enterPassword("Test@123");
+        register.enterConfirmPassword("Test@123");
+        register.clickRegisterButton();
+
+        Assert.assertTrue(
+                register.getDuplicateEmailError().contains("already exists"),
+                "Duplicate email validation failed");
+    }
+
+    // TC_REG_06 – Password Mismatch
+    @Test()
+    public void TC_REG_06_passwordMismatch() {
+
+        register = new RegisterPageDesign(driver);
+
+        register.clickRegisterLink();
+        register.selectFemaleGender();
+        register.enterFirstName("Hana");
+        register.enterLastName("Louis");
+        register.enterEmail("hana.mismatch" + System.currentTimeMillis() + "@testmail.com");
+        register.enterPassword("Test@123");
+        register.enterConfirmPassword("Test@124");
+        register.clickRegisterButton();
+
+        Assert.assertTrue(
+                register.getPasswordMismatchError().contains("do not match"),
+                "Password mismatch validation failed");
+    }
+
+    // TC_REG_07 – Short Password
+    @Test
+    public void TC_REG_07_shortPassword() {
+
+        register = new RegisterPageDesign(driver);
+
+        register.clickRegisterLink();
+        register.selectFemaleGender();
+        register.enterFirstName("Hana");
+        register.enterLastName("Louis");
+        register.enterEmail("hana.shortpwd" + System.currentTimeMillis() + "@testmail.com");
+        register.enterPassword("123");
+        register.enterConfirmPassword("123");
+        register.clickRegisterButton();
+
+        Assert.assertTrue(
+                register.getPasswordLengthError().contains("should have at least"),
+                "Password length validation failed");
+    }
+}
